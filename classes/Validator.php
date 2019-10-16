4bb9494468f08501b8a9a52eb1b7de8cde57c806
Ehab Reda <ehabred04@gmail.com>
2019-10-16 17:14:06 +0200

    adding validations

---
+++ classes/Validator.php
@@ -0,0 +1,42 @@
+<?php 
+   /**
+    * summary
+    */
+   class Validator
+   {
+       /*
+   * check requred filed
+  */
+  public function isRequered($filed_array){
+   foreach ($filed_array as $filed) {
+      if($_POST[''.$filed.''] == ''){
+        return false;
+      }
+    }
+    return true; 
+  }
+
+
+  /*
+   * validate email field
+  */
+
+  public function isvalidemail($email){
+    if (filter_var($email,FILTER_VALIDATE_EMAIL)) {
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  /*
+   * check pass match
+  */
+  public function passmatch($pass1,$pass2){
+    if ($pass1 == $pass2) {
+      return true;
+    }else{
+      return false;
+    }
+  }
+   }
--- register.php
+++ register.php
@@ -2,6 +2,7 @@
 <?php 
    $template=new Template('Templates/register.php');
    $user=new User();
+   $valed=new Validator();
    if (isset($_POST['register'])) {
    	    $data = array();
 	    $data['name'] = $_POST['name'];
@@ -11,21 +12,38 @@
 	    $data['password2'] = $_POST['password2'];
 	    $data['about'] = $_POST['about'];
 	    $data['last_actvaty'] = date("y-m-d H:i:s");
-	    if ($user->uploadavatar()) {
-		   $data['avatar'] = $_FILES["avatar"]["name"];
-		 }
-		else{
-			$data['avatar'] = 'noimge.png';
-		 }
-		if ($user->register($data)) {
-	    	redirect('index.php','success registrition you can log in now','success');
-	     }
-	    else{
-	       redirect('index.php','some thing goes wrong please try agin','fial');
-	       }
-   	   // else{
-   	   // 	    redirect('register.php','password doesnot match','error');
-   	   //     }
+	      //check for validation 
+		   $array_valid = array('name','email','username','password','password2');
+		   if ($valed->isRequered($array_valid)) {
+		   	   if($valed->isvalidemail($data['email'])){
+		   	   	   if($valed->passmatch($data['password'],$data['password2'])){
+		   	   	   	//uploaded avatar
+						    if ($user->uploadavatar()) {
+						    	$data['avatar'] = $_FILES["avatar"]["name"];
+						    }
+						    else{
+						    	$data['avatar'] = 'noimge.png';
+						    }
+
+						    if ($user->register($data)) {
+						    	redirect('index.php','success registrition you can log in now','success');
+						    }
+						    else{
+						       redirect('index.php','some thing goes wrong please try again','fail');
+						    }
+
+		   	   	   }else{
+		   	   	   	    redirect('register.php','password doesnot match','error');
+
+		   	   	   }
+
+		   	   }else{
+
+		           redirect('register.php','in valid email','error');
+		   	   }
+		   }else{
+		       redirect('register.php','fill all input please ','error');
+		   }
    	 }  
 
 
